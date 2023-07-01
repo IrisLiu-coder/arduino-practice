15032169473ee457c860e4563a5bcf2b4fe2e94d
劉邦均 <83829881+IrisLiu-coder@users.noreply.github.com>
2023-07-01 15:36:04 +0800

    20230701

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,11 +1,20 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo servo_1;  // create servo object to control a servo
+Servo servo_2;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  servo_1.attach(9); 
+  servo_2.attach(10); 
+  servo_1.write(0);
+  servo_2.write(0);
 }
 
 void loop() {
-  myservo.write(90);         
+  servo_1.write(180);    
+  servo_2.write(180);
+  delay(1000);
+  servo_1.write(0); 
+  servo_2.write(0);   
+  delay(1000);     
 }
